4a125b4986fe89d18b0c2940f2b3d143950a6fbc
Jorge Rivero <jlrivero85@gmail.com>
2023-07-29 17:17:00 -0400

    Initial commit

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-from collections import Counter
-from typing import List, Tuple
+from collections import Counter, defaultdict
+from typing import List, Tuple, Dict
+
 
 users_interests = [
     ["Hadoop", "Big Data", "HBase", "Java", "Spark", "Storm", "Cassandra"],
@@ -34,10 +35,7 @@ def most_popular_new_interests(user_interests: List[str],
     return suggestions[:max_results]
 
 
-most_popular_new_interests(users_interests)
-
 # 2. User-Based Collaborative Filtering
-
 unique_interests = sorted({interest
                            for user_interests in users_interests
                            for interest in user_interests})
@@ -58,5 +56,29 @@ user_similarities = [[cosine_similarity(interest_vector_i, interest_vector_j)
                      for interest_vector_i in user_interest_vectors]
 
 
-print(len(user_interest_vectors))
-print(len(user_similarities))
+def most_similar_users_to(user_id: int)->List[Tuple]:
+    pairs = [(user, similarity) for user,similarity in enumerate(user_similarities[user_id]) if user != user_id and similarity > 0]
+    return sorted(pairs, key=lambda pair: pair[-1], reverse=True)
+
+def user_based_suggestions(user_id: int, include_current_interests: bool = False):
+    # Sum up the similarities
+    suggestions: Dict[str, float] = defaultdict(float)
+    print(most_similar_users_to(user_id))
+    for other_user_id, similarity in most_similar_users_to(user_id):
+        for interest in users_interests[other_user_id]:
+            suggestions[interest] += similarity
+
+    # Convert them to a sorted list
+    suggestions = sorted(suggestions.items(),
+                         key=lambda pair: pair[-1],  # weight
+                         reverse=True)
+
+    # And (maybe) exclude already interests
+    if include_current_interests:
+        return suggestions
+    else:
+        return [(suggestion, weight)
+                for suggestion, weight in suggestions
+                if suggestion not in users_interests[user_id]]
+
+print(user_based_suggestions(1))
